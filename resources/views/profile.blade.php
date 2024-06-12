e4dc862285c604e77ce3077ce7a262e0bf07a26e
Levan Gogoladze <levanma98@gmail.com>
2024-06-12 15:11:03 +0400

    Add few changes

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,73 +1,75 @@
 @extends('layout')
 @section('title', 'Profile')
 @section('content')
-    <div class="container">
-        <div class="main-body">
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="d-flex flex-column align-items-center text-center">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="Admin"
-                                     class="rounded-circle p-1 bg-primary" width="110">
-                                <div class="mt-3">
-                                    <h4>{{$user->full_name}}</h4>
-                                </div>
+    <h1 class="heading-1 text-center" style="margin-top: 50px; margin-bottom: 20px">
+        Profile
+    </h1>
+
+    <div class="main-body">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex flex-column align-items-center text-center">
+                            <img src="{{assert('/assets/img/user.png')}}" alt="User"
+                                 class="rounded-circle p-1 bg-primary" width="110">
+                            <div class="mt-3">
+                                <h4>{{$user->full_name}}</h4>
                             </div>
-                            <hr class="my-4">
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                                    <h6 class="mb-0">
-                                        Company Name
-                                    </h6>
-                                    <span class="text-secondary">{{$user->company_name}}</span>
-                                </li>
-                            </ul>
                         </div>
+                        <hr class="my-4">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                <h6 class="mb-0">
+                                    Company Name
+                                </h6>
+                                <span class="text-secondary">{{$user->company_name}}</span>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-                <div class="col-lg-8">
-                    <div class="card">
-                        <div class="card-body">
-                            <form action="{{route('update.profile')}}" method="POST">
-                                @csrf
-                                @method('PUT')
-                                <div class="row mb-3">
-                                    <div class="col-sm-3">
-                                        <h6 class="mb-0">Full Name</h6>
-                                    </div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <input type="text" name="full_name" class="form-control"
-                                               value="{{old('full_name', $user->full_name)}}">
-                                    </div>
+            </div>
+            <div class="col-lg-8">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="{{route('update.profile')}}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="row mb-3">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">Full Name</h6>
                                 </div>
-                                <div class="row mb-3">
-                                    <div class="col-sm-3">
-                                        <h6 class="mb-0">Email</h6>
-                                    </div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <input type="text" name="email" class="form-control"
-                                               value="{{old('email', $user->email)}}">
-                                    </div>
+                                <div class="col-sm-9 text-secondary">
+                                    <input type="text" name="full_name" class="form-control"
+                                           value="{{old('full_name', $user->full_name)}}">
                                 </div>
-                                <div class="row mb-3">
-                                    <div class="col-sm-3">
-                                        <h6 class="mb-0">Company Name</h6>
-                                    </div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <input type="text" name="company_name" class="form-control"
-                                               value="{{old('company_name', $user->company_name)}}">
-                                    </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">Email</h6>
                                 </div>
+                                <div class="col-sm-9 text-secondary">
+                                    <input type="text" name="email" class="form-control"
+                                           value="{{old('email', $user->email)}}">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">Company Name</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    <input type="text" name="company_name" class="form-control"
+                                           value="{{old('company_name', $user->company_name)}}">
+                                </div>
+                            </div>
 
-                                <div class="row">
-                                    <div class="col-sm-3"></div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <input type="submit" class="btn btn-primary px-4" value="Save Changes">
-                                    </div>
+                            <div class="row">
+                                <div class="col-sm-3"></div>
+                                <div class="col-sm-9 text-secondary">
+                                    <input type="submit" class="btn btn-primary px-4" value="Save Changes">
                                 </div>
-                            </form>
-                        </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
